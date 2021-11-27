58ceca46bdd28ac65a9aa575f964e290c19bbd2b
Alforsan <Alforsan@DESKTOP-URCU90U>
2021-11-27 10:52:16 +0200

    Add project files.

---
+++ Reverse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AmirReversed
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> superhero = new LinkedList<string>(); ;
+
+            LinkedListNode<string> spiderman = superhero.AddFirst("tom holland");
+
+            superhero.AddLast("Tobey Maguire");
+            superhero.AddAfter(spiderman, "Garfield");
+            superhero.AddBefore(spiderman, "Jake Johnson");
+            superhero.AddLast("Drake Bell");
+            Console.WriteLine("The linked List Before Reversing: \n ");
+            foreach (var item in superhero)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("The linked List After Reversing: \n ");
+            foreach (var item in superhero.Reverse())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+    }
+}
